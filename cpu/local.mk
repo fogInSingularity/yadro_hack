6e524391488f939d02ddb481f86774651205b840
nniikon <kolya.kasparov@gmail.com>
2026-04-19 01:09:54 +0300

    update local.mk

--- cpu/local.mk
+++ cpu/local.mk
@@ -1,70 +1,68 @@
-#[asm/c]
-PROG_TYPE?=asm
+PROG_NAME ?= display
+IMEM_WORDS_CNT ?= 256
 
-PROG_NAME?=fib_fpga
+RISCV_PREFIX := riscv64-unknown-elf-
 
-IMEM_WORDS_CNT?=4096
-# ---------------------------------
+AS      := $(RISCV_PREFIX)as
+CC      := $(RISCV_PREFIX)gcc
+OBJCOPY := $(RISCV_PREFIX)objcopy
+OBJDUMP := $(RISCV_PREFIX)objdump
+READELF := $(RISCV_PREFIX)readelf
 
-ifneq ($(PROG_TYPE),asm)
-ifneq ($(PROG_TYPE),c)
-$(error "WRONG PROG_TYPE! MUST BE ONE OF: asm, c.")
-endif
-endif
+PROGS_DIR      := $(TASK_DIR)/c_megaprog
+OUT_DIR        := $(TASK_DIR)/out_prog
+ASM_COMMON_DIR := $(TASK_DIR)/asm_common
+SCRIPTS_DIR    := $(TASK_DIR)/scripts
+SCRIPT_NOP_PAD := $(SCRIPTS_DIR)/nop_pad.py
+SCRIPT_TXT2MIF := $(SCRIPTS_DIR)/txt2mif.py
 
-RISCV_PREFIX:=riscv64-unknown-elf-
+C_SRCS := $(wildcard $(PROGS_DIR)/*.c)
+C_OBJS := $(patsubst $(PROGS_DIR)/%.c,$(OUT_DIR)/%.o,$(C_SRCS))
+C_DEPS := $(C_OBJS:.o=.d)
 
-AS=$(RISCV_PREFIX)as
-LD=$(RISCV_PREFIX)ld
-CC=$(RISCV_PREFIX)gcc
-OBJCOPY=$(RISCV_PREFIX)objcopy
-OBJDUMP=$(RISCV_PREFIX)objdump
-READELF=$(RISCV_PREFIX)readelf
+COMMON_RV_FLAGS := -march=rv32i -mabi=ilp32
+BARE_FLAGS      := -ffreestanding -nostdlib -nostartfiles
+NO_GOT_FLAGS    := -fno-pic -fno-pie -msmall-data-limit=0
 
-PROGS_DIR:=$(TASK_DIR)/$(PROG_TYPE)_progs
-OUT_DIR:=$(TASK_DIR)/out_prog
-ASM_COMMON_DIR:=$(TASK_DIR)/asm_common
-SCRIPTS_DIR:=$(TASK_DIR)/scripts
-SCRIPT_NOP_PAD:=$(SCRIPTS_DIR)/nop_pad.py
-SRCIPT_TXT2MIF:=$(SCRIPTS_DIR)/txt2mif.py
+CFLAGS  := $(COMMON_RV_FLAGS) $(BARE_FLAGS) $(NO_GOT_FLAGS) -Oz -flto
+LDFLAGS := $(COMMON_RV_FLAGS) $(BARE_FLAGS) $(NO_GOT_FLAGS) -Oz -flto
+LDSCRIPT := $(ASM_COMMON_DIR)/riscv.ld
 
 # ---------------------------------
 
 $(OUT_DIR)/%.txt: $(OUT_DIR)/%.bin
-	hexdump -v -e '"%08x\n"' $^ > $@
+	hexdump -v -e '"%08x\n"' $< > $@
 	python3 $(SCRIPT_NOP_PAD) $@ $(IMEM_WORDS_CNT)
 
 $(OUT_DIR)/%.mif: $(OUT_DIR)/%.txt
-	python3 $(SRCIPT_TXT2MIF) $^ $@
+	python3 $(SCRIPT_TXT2MIF) $< $@
 
 $(OUT_DIR)/%.bin: $(OUT_DIR)/%.out
-	$(OBJCOPY) -j .text -O binary $^ $@
+	$(OBJCOPY) -j .text -O binary $< $@
 
 $(OUT_DIR)/loader.o: $(ASM_COMMON_DIR)/loader.s
 	@mkdir -p $(OUT_DIR)
-	$(AS) -march=rv32i -mabi=ilp32 -c $(ASM_COMMON_DIR)/loader.s -o $(OUT_DIR)/loader.o
+	$(AS) $(COMMON_RV_FLAGS) -c $< -o $@
 
-ifeq ($(PROG_TYPE),asm)
-$(OUT_DIR)/%.out: $(PROGS_DIR)/%.s
-	@mkdir -p $(OUT_DIR) 
-	$(AS) -march=rv32i -mabi=ilp32 -c $^ -o $(OUT_DIR)/$*.o
-	$(LD) -melf32lriscv -Map=$(OUT_DIR)/$*.map \
-	-T $(ASM_COMMON_DIR)/riscv.ld $(OUT_DIR)/$*.o -o $@
-	$(OBJDUMP) -d --visualize-jumps -M no-aliases $@ | tee $(OUT_DIR)/$*.dis
-else ifeq ($(PROG_TYPE),c)
-$(OUT_DIR)/%.out: $(PROGS_DIR)/%.c $(OUT_DIR)/loader.o
-	@mkdir -p $(OUT_DIR) 
-	$(CC) -nostdlib -march=rv32i -mabi=ilp32 -fPIC -c $< -o $(OUT_DIR)/$*.o
-	$(LD) -melf32lriscv -Map=$(OUT_DIR)/$*.map \
-	-T $(ASM_COMMON_DIR)/riscv.ld $(OUT_DIR)/loader.o $(OUT_DIR)/$*.o -o $@
-	$(OBJDUMP) -d --visualize-jumps -M no-aliases $@ | tee $(OUT_DIR)/$*.dis
-endif
+$(OUT_DIR)/%.o: $(PROGS_DIR)/%.c
+	@mkdir -p $(OUT_DIR)
+	$(CC) $(CFLAGS) -MMD -MP -c $< -o $@
+
+$(OUT_DIR)/$(PROG_NAME).out: $(OUT_DIR)/loader.o $(C_OBJS)
+	@mkdir -p $(OUT_DIR)
+	$(CC) $(LDFLAGS) \
+		-Wl,-Map=$(OUT_DIR)/$(PROG_NAME).map \
+		-T $(LDSCRIPT) \
+		$(OUT_DIR)/loader.o $(C_OBJS) -o $@
+# 	$(OBJDUMP) -d --visualize-jumps -M no-aliases $@ | tee $(OUT_DIR)/$(PROG_NAME).dis
 
 .PHONY: build_prog
 build_prog: $(OUT_DIR)/$(PROG_NAME).txt $(OUT_DIR)/$(PROG_NAME).mif
-	@echo "Result hex file: " $^
+	@echo "Result hex file: " $(OUT_DIR)/$(PROG_NAME).txt $(OUT_DIR)/$(PROG_NAME).mif
 	@echo "Result disasm file: " $(OUT_DIR)/$(PROG_NAME).dis
 
 .PHONY: clean_out
 clean_out:
 	rm -rf $(OUT_DIR)
+
+-include $(C_DEPS)
